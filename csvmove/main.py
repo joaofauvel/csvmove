e0c761dc7463d8b49b6c7eb52a40d5b3736eeb8e
João Murilo Palone Fauvel <joaofauvel@FMT.MT>
2023-06-07 19:12:09 -0400

    file check and rename if exists

--- csvmove/main.py
+++ csvmove/main.py
@@ -12,19 +12,35 @@ def move_files(input_csv: Path, path_col: str, cluster_col: str, copy: bool, out
     # Reading the csv file using pandas
     df = pd.read_csv(input_csv)
     # Looping over the rows of the dataframe
-    for row in track(df.itertuples(), description="Moving files..."):
+    desc = "Copying files..." if copy else "Moving files..."
+    for row in track(df.itertuples(), description=desc):
         # Getting the file path and cluster from the row
         file_path = Path(getattr(row, path_col))
         cluster = getattr(row, cluster_col)
         # Creating a subdirectory for the cluster if it does not exist
         cluster_dir = output_dir / cluster
         cluster_dir.mkdir(exist_ok=True)
-        if copy:
-            # Copying the file to the cluster subdirectory
-            shutil.copy(file_path, (cluster_dir / file_path.name))
+        destination = cluster_dir / file_path.name
+        if file_path.exists():
+            if destination.exists():
+                stem = destination.stem
+                suffix = destination.suffix
+                counter = 1
+                while True:
+                    new_stem = f"{stem}-{counter}"
+                    destination = destination.with_name(new_stem + suffix)
+                    if destination.exists():
+                        counter+=1
+                    else:
+                        break
+            if copy:
+                # Copying the file to the cluster subdirectory
+                shutil.copy(file_path, destination)
+            else:
+                # Moving the file to the cluster subdirectory
+                file_path.rename(destination)
         else:
-            # Moving the file to the cluster subdirectory
-            file_path.rename(cluster_dir / file_path.name)
+            pass
 
 # Defining a command for the app that takes the arguments from the user
 @app.command()
@@ -37,3 +53,5 @@ def run(
 ):
     # Calling the move_files function with the arguments
     move_files(input_file, path, cluster, copy, output)
+
+# TODO Handle duplicate files in the destination
